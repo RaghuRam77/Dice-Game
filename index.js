b97b5bec3af92d37d720b1663d81cae5f02b207d
RaghuRam77 <64365636+RaghuRam77@users.noreply.github.com>
2021-09-12 16:36:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1= Math.floor(Math.random() *6+1);
+var randomDiceimage="dice"+randomNumber1+".png";
+var randomimagesource="images/"+randomDiceimage;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomimagesource);
+
+var randomNumber2= Math.floor(Math.random() *6+1);
+var randomDiceimage="dice"+randomNumber2+".png";
+var randomimagesource="images/"+randomDiceimage;
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomimagesource);
+
+if(randomNumber1 > randomNumber2)
+{
+     document.querySelector("h1").innerHTML="🚩Player 1 Wins"
+}
+else if(randomNumber2 > randomNumber1)
+{
+    document.querySelector("h1").innerHTML="🚩Player 2 Wins"
+}
+else
+{
+    document.querySelector("h1").innerHTML="Draw!"
+}
